c4404eb888f197c0de992e629f7fb6943359e8e4
Sirbolu <peterbolu@yahoo.com>
2026-02-20 12:40:11 +0100

    button event listener

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const advice = document.querySelector(".quote");
 const number = document.querySelector(".heading-number");
+const dice = document.querySelector(".dice");
+
+advice.innerHTML = "Click the dice button to generate advice.";
+// number.innerHTML = "";
 
 const getAdvice = async () => {
   try {
+    // Show loading instantly
+    advice.innerHTML = "Loading...";
+    number.innerHTML = "...";
+
     const adviceDataFetch = await fetch("https://api.adviceslip.com/advice");
+    // const adviceDataFetch = await fetch(`https://api.adviceslip.com/advice`);
     const adviceData = await adviceDataFetch.json();
     console.log(adviceData);
     number.innerHTML = `${adviceData.slip.id}`;
@@ -13,4 +22,4 @@ const getAdvice = async () => {
   }
 };
 
-getAdvice();
+dice.addEventListener("click", getAdvice);
